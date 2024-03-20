63c7724550c92406fa31688013e1ea532c92eafd
emmanuel <emmanuel.kisoso@student.moringaschool.com>
2024-03-20 22:56:21 +0300

    Intial Commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqlocation = 42;
+    return Math.abs(someValue-hqlocation);
+
+}
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+}
+function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock=264;
+    const blocksCovered=Math.abs(destination-start)
+    const feetTraveled=blocksCovered*feetPerBlock
+    return feetTraveled
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance<=400) {
+        return 0;
+    }
+    else if (distance>400 && distance<2000) {
+        return (distance-400)*0.02;
+    }
+    else if (distance>2000 && distance<2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
+
+
+
+
+
+
